392ba5a2c6cd3fa2e55123288d345734bbb9c2c0
ICY <1225260298@qq.com>
2023-09-27 14:44:19 +0800

    update

--- LP/assets/go.js
+++ LP/assets/go.js
@@ -1,6 +1,6 @@
 const bbrUrl = "https://ftwxib.mcgo2.com/visit/6c10f194-8aff-41e6-8943-e9911886deb1?c1=bbr"
 const pushUrl = "https://ftwxib.mcgo2.com/visit/2bb9208f-c940-4961-9ea3-596e9f627722?c1=push"
-const underUrl = "https://hewh.com.cn/push/index.html"
+const underUrl = "https://hewh.com.cn/LP/home/index.html"
 const clickUrl = "https://ftwxib.mcgo2.com/click"
 function toOffer() {
 	window.open(clickUrl)
