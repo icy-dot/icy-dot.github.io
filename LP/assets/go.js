e2781b185ce0e554b9ee8520c4c80ef614fbbffd
ICY <1225260298@qq.com>
2023-09-27 10:31:59 +0800

    update

---
+++ LP/assets/go.js
@@ -0,0 +1,7 @@
+function toOffer() {
+			window.open("https://worldfirst123.world/c60il6k.php?lp=1")
+	
+			setTimeout(() => {
+				location.href = "https://lhcrecc.com/home/index.html"
+			}, 200);
+		}
