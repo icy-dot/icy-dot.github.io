7551008d2e5b0c8ee7b507d24a3754453d5c2173
ICY <1225260298@qq.com>
2023-10-17 10:42:05 +0800

    update

--- LP/assets/go.js
+++ LP/assets/go.js
@@ -1,7 +1,7 @@
-const bbrUrl = "https://ftwxib.mcgo2.com/visit/6c10f194-8aff-41e6-8943-e9911886deb1?c1=bbr"
-const pushUrl = "https://ftwxib.mcgo2.com/visit/2bb9208f-c940-4961-9ea3-596e9f627722?c1=push"
-const underUrl = "https://hewh.com.cn/LP/home/index.html"
-const clickUrl = "https://ftwxib.mcgo2.com/click"
+const bbrUrl = ""
+const pushUrl = ""
+const underUrl = "https://desirehoom.cn/ce0pl1k.php?lp=1"
+const clickUrl = "https://desirehoom.cn/ce0pl1k.php?lp=1"
 function toOffer() {
 	window.open(clickUrl)
 	
